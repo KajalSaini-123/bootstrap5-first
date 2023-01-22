e5dc81cf0511655e7dda41152591ec6a60e0956a
unknown <tony0561497@gmail.com>
2023-01-22 23:26:30 +0530

    owl carousal add

---
+++ js/app.js
@@ -0,0 +1,20 @@
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    items: 1,
+    dots:false,
+    navText: ['PREV', 'NEXT'],
+    responsive: {
+        0: {
+      
+        },
+        600: {
+        
+        },
+        1000: {
+        
+        }
+    }
+})
