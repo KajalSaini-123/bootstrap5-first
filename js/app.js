1542bb39704cc88b30e9d0f3288d4f88e4fccbae
unknown <tony0561497@gmail.com>
2023-01-23 22:56:18 +0530

    about section

--- js/app.js
+++ js/app.js
@@ -5,6 +5,9 @@ $('.owl-carousel').owlCarousel({
     nav: true,
     items: 1,
     dots:false,
+    smartSpeed: 1000,
+    autoplay: true,
+    autoplayTimeout: 7000,
     navText: ['PREV', 'NEXT'],
     responsive: {
         0: {
