a2372f2d6489f483f657229f12889e94031248c1
unknown <tony0561497@gmail.com>
2023-01-26 22:22:30 +0530

    work and team section

--- js/app.js
+++ js/app.js
@@ -1,23 +1,43 @@
-
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 0,
-    nav: true,
-    items: 1,
-    dots:false,
-    smartSpeed: 1000,
-    autoplay: true,
-    autoplayTimeout: 7000,
-    navText: ['PREV', 'NEXT'],
-    responsive: {
-        0: {
-      
-        },
-        600: {
-        
-        },
-        1000: {
-        
-        }
-    }
-})
+// ------FIRST CAROUSAL----------
+$("#first-slider").owlCarousel({
+  loop: true,
+  margin: 0,
+  nav: true,
+  items: 1,
+  dots: false,
+  smartSpeed: 1000,
+  autoplay: true,
+  autoplayTimeout: 7000,
+  navText: ["PREV", "NEXT"],
+  responsive: {
+    0: {
+      nav: false,
+    },
+    600: {
+      nav: true,
+    },
+  },
+});
+$("#projects-slider").owlCarousel({
+  loop: true,
+  margin: 0,
+  dots: true,
+  margin: 24,
+  dots: true,
+  smartSpeed: 1000,
+  autoplay: true,
+  autoplayTimeout: 7000,
+  center: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1140: {
+      items: 2,
+      center: true,
+    },
+  },
+});
